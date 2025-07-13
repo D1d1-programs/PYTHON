a15253ea424a7d2c8f90eda7514ec034d0afdc66
D1d1-programs <itaedidiong11@gmail.com>
2025-07-13 19:30:32 +0100

    Add files via upload

---
+++ Python Acps/storing birthdays.py
@@ -0,0 +1,13 @@
+# variable
+
+Ella = '19/02/2012'
+Joyce = '21/06/2012'
+Ifiok = '14/09/2015'
+Kaobi = '23/12/2012'
+Danielle = '4/09/2012'
+
+print(Ella)
+print(Joyce)
+print(Ifiok)
+print(Kaobi)
+print(Danielle)
